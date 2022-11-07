98c9d142ebd480ff34353c2d1ad0f4def54e3efe
KimberlyPocco <kimberly.poccop@gmail.com>
2022-11-06 22:48:14 -0500

    crear funcion listar por habitad y submenu habitad

--- tarea2.py
+++ tarea2.py
@@ -39,3 +39,24 @@ def subMenuForma():
     detalleSolicitud : dict =resultado.json()
     resultado= [forma ['name'] for forma in detalleSolicitud['results'] ]
     return resultado
+
+
+
+#LISTAR POR HABITAT
+def listarPokemonHabitat(habitat):
+    try:
+        resultado = requests.get(f'https://pokeapi.co/api/v2/pokemon-habitat/{habitat}')
+        detalleSolicitud: dict =resultado.json()
+        resultado= {'nombre': [ habitad ['name'] for habitad in detalleSolicitud['pokemon_species']],
+                    'habilidad': [],
+                    'url': [url ['url'] for url in detalleSolicitud['pokemon_species']]}
+        return resultado
+    except:
+        print(rojo+"NO EXISTE LA HABITAD INGRESADA"+reset)
+
+#SUBMENU DE HABITAD
+def subMenuHabitad():
+    resultado=requests.get(f'https://pokeapi.co/api/v2/pokemon-habitat/')
+    detalleSolicitud : dict =resultado.json()
+    resultado= [habitad ['name'] for habitad in detalleSolicitud['results'] ]
+    return resultado
