0cd8a67b1a9eebd508868fafd6f3e32b6806f821
Gian <gianfrancopariona9@gmail.com>
2022-11-06 21:10:15 -0500

    Crear destructor de la clase libro

---
+++ tarea1.py
@@ -0,0 +1,18 @@
+#Crear la clase libro
+class Libro:
+    def __init__(self,id, titulo, genero, ISBN, editorial,autores):
+        self.id=id
+        self.titulo=titulo
+        self.genero=genero
+        self.ISBN=ISBN
+        self.editorial=editorial
+        self.autores=autores
+        self.cantAutores=1
+        for i in self.autores:
+            if i == '&':
+                self.cantAutores+=1
+                
+#crear Destructor de Clase Libro
+    def __del__(self) :
+        print("\tLibro eliminado")
+        return None
