c52e07edc6334346b64e085bcf4c65f8b74dcd03
KimberlyPocco <kimberly.poccop@gmail.com>
2022-11-06 22:31:41 -0500

    listar por forma de pokemon

--- tarea2.py
+++ tarea2.py
@@ -8,3 +8,28 @@ morado = '\033[35m'
 celeste = '\033[36m'
 blanco = '\033[37m'
 reset = '\033[39m'
+
+#LISTAR POR GENERACION 
+def listarPokemonGeneration(generation):
+    try: 
+        resultado= requests.get(f'https://pokeapi.co/api/v2/generation/{generation}')
+        detalleSolicitud : dict= resultado.json()
+        resultado ={'nombre' :  [ nombres ['name'] for nombres in detalleSolicitud['pokemon_species']],
+                    'habilidad': [ detalleSolicitud['abilities']],
+                    'url' : [url ['url'] for url in detalleSolicitud['pokemon_species']]}
+        return resultado
+    except :
+        print(rojo+"NO EXISTE LA GENERACIÓN INGRESADA"+reset) 
+
+
+#LISTAR POR FORMA
+def listarPokemonForma(shape):
+    try:
+        resultado=requests.get(f'https://pokeapi.co/api/v2/pokemon-shape/{shape}')
+        detalleSolicitud: dict =resultado.json()
+        resultado={'nombre' : [nombres ['name'] for nombres in detalleSolicitud['pokemon_species']],
+                 'habilidad' : [],
+                 'url': [nombres ['url'] for nombres in detalleSolicitud['pokemon_species']]}
+        return resultado
+    except :
+            print(rojo+"NO EXITEN POKEMONES CON LA FORMA INGRESADA"+reset)
